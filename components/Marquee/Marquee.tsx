8a13c2a8a4b61066a5072fd0f99ac6dbe6cd750c
Lukas Gabsi <lukas.gabsi@gmail.com>
2024-09-27 17:14:22 +0200

    fix marquee

--- components/Marquee/Marquee.tsx
+++ components/Marquee/Marquee.tsx
@@ -92,7 +92,7 @@ export function VelocityScroll({
         className="w-full overflow-hidden whitespace-nowrap"
         ref={containerRef}
       >
-        <motion.div className={`inline-block ${className}`} style={{ x }}>
+        <motion.div className={`inline-block ${className}`} style={{ transform: `translateX(${x})` }}>
           {Array.from({ length: repetitions }).map((_, i) => (
             <span key={i} ref={i === 0 ? textRef : null} className={'text-primary bg-secondary'}>
               {children}{" "}
