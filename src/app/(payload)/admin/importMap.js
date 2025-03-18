ba01d0f5e97522de3e827fa5c726d34e4949665b
Lukas Gabsi <lukas.gabsi@gmail.com>
2025-03-18 21:49:36 +0100

    update importmap

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,5 +1,5 @@
-import { default as default_379affef2f3ef2e2ff61389ad0f9cbb2 } from '@/fields/Projects/ContentEditField.tsx'
+
 
 export const importMap = {
-  "@/fields/Projects/ContentEditField.tsx#default": default_379affef2f3ef2e2ff61389ad0f9cbb2
+
 }
