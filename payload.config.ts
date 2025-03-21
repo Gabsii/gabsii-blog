799d3a186f54099f96d31c846bd588fd75eb9985
Lukas Gabsi <lukas.gabsi@gmail.com>
2025-03-21 19:27:19 +0100

    fix config

--- payload.config.ts
+++ payload.config.ts
@@ -23,10 +23,10 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  auth: {
-    maxLoginAttempts: 5, // Automatically lock a user out after X amount of failed logins
-    lockTime: 60 * 60 * 100, // Time period to allow the max login attempts in seconds
-  },
+  // auth: {
+    // maxLoginAttempts: 5, // Automatically lock a user out after X amount of failed logins
+    // lockTime: 60 * 60 * 100, // Time period to allow the max login attempts in seconds
+  // },
   localization: {
     locales: ["en", "de"],
     defaultLocale: "en",
